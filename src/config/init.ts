aa4b256ef8a152b1146f161068dcae572f85a0d9
SergeyL-club <damnazzel@gmail.com>
2023-11-20 16:44:11 +0500

    fix language

--- src/config/init.ts
+++ src/config/init.ts
@@ -5,57 +5,57 @@ import {logger} from "../utils/logger";
 import {env} from "./env";
 
 export const init = async () => {
-  logger.info("???????? ?????");
+  logger.info("Установка базовых данных");
 
   let roleCandidate = await RoleModel.findOne({search: [{key: "name", value: "admin"}]});
   if (!roleCandidate) {
-    logger.info(`???????? ???? "admin"`);
-    await RoleModel.add({name: "admin", description: "?????? ? ????? ???????", functions: ["main", "users", "roles", "transactions"]});
-  } else logger.warn('???? "admin" ??? ??????????');
+    logger.info(`Создание роли "admin"`);
+    await RoleModel.add({name: "admin", description: "Работа с админ панелью", functions: ["main", "users", "roles", "transactions"]});
+  } else logger.warn('Роль "admin" уже существует');
 
   roleCandidate = await RoleModel.findOne({search: [{key: "name", value: "trader"}]});
   if (!roleCandidate) {
-    logger.info(`???????? ???? "trader"`);
-    await RoleModel.add({name: "trader", description: "?????? ?? ????????", functions: ["main"]});
-  } else logger.warn('???? "trader" ??? ??????????');
+    logger.info(`Создание роли "trader"`);
+    await RoleModel.add({name: "trader", description: "Работа с заявками", functions: ["main"]});
+  } else logger.warn('Роль "trader" уже существует');
 
   roleCandidate = await RoleModel.findOne({search: [{key: "name", value: "partner"}]});
   if (!roleCandidate) {
-    logger.info(`???????? ???? "partner"`);
-    await RoleModel.add({name: "partner", description: "?????????????? ??????", functions: ["main"]});
-  } else logger.warn('???? "partner" ??? ??????????');
+    logger.info(`Создание роли "partner"`);
+    await RoleModel.add({name: "partner", description: "Предоставление заявок", functions: ["main"]});
+  } else logger.warn('Роль "partner" уже существует');
 
   roleCandidate = await RoleModel.findOne({search: [{key: "name", value: "operator"}]});
   if (!roleCandidate) {
-    logger.info(`???????? ???? "operator"`);
-    await RoleModel.add({name: "operator", description: "?????? ?? ???????", functions: ["main"]});
-  } else logger.warn('???? "operator" ??? ??????????');
+    logger.info(`Создание роли "operator"`);
+    await RoleModel.add({name: "operator", description: "Работа со спорными заявками", functions: ["main"]});
+  } else logger.warn('Роль "operator" уже существует');
 
   let candidate = await UserModel.findOne({search: [{key: "login", value: env.INIT_ADMIN_LOGIN}]});
   if (!candidate) {
-    logger.info(`???????? ???????????? "${env.INIT_ADMIN_LOGIN}"(admin)`);
+    logger.info(`Создание пользователя "${env.INIT_ADMIN_LOGIN}"(admin)`);
     let roleAdmin = await RoleModel.findOne({search: [{key: "name", value: "admin"}]});
     await UserModel.add({login: env.INIT_ADMIN_LOGIN, passwordHash: hashSync(env.INIT_ADMIN_PASSWORD, env.SALT_ROUND), roleId: roleAdmin!.id});
-  } else logger.warn(`???????????? "${env.INIT_ADMIN_LOGIN}"(admin) ??? ??????????`);
+  } else logger.warn(`Пользователь "${env.INIT_ADMIN_LOGIN}"(admin) уже существует`);
 
   candidate = await UserModel.findOne({search: [{key: "login", value: env.INIT_TRADER_LOGIN}]});
   if (!candidate) {
-    logger.info(`???????? ???????????? "${env.INIT_TRADER_LOGIN}"(trader)`);
+    logger.info(`Создание пользователя "${env.INIT_TRADER_LOGIN}"(trader)`);
     let roleAdmin = await RoleModel.findOne({search: [{key: "name", value: "trader"}]});
     await UserModel.add({login: env.INIT_TRADER_LOGIN, passwordHash: hashSync(env.INIT_TRADER_PASSWORD, env.SALT_ROUND), roleId: roleAdmin!.id});
-  } else logger.warn(`???????????? "${env.INIT_TRADER_LOGIN}"(trader) ??? ??????????`);
+  } else logger.warn(`Пользователь "${env.INIT_TRADER_LOGIN}"(trader) уже существует`);
 
   candidate = await UserModel.findOne({search: [{key: "login", value: env.INIT_PARTNER_LOGIN}]});
   if (!candidate) {
-    logger.info(`???????? ???????????? "${env.INIT_PARTNER_LOGIN}"(partner)`);
+    logger.info(`Создание пользователя "${env.INIT_PARTNER_LOGIN}"(partner)`);
     let roleAdmin = await RoleModel.findOne({search: [{key: "name", value: "partner"}]});
     await UserModel.add({login: env.INIT_PARTNER_LOGIN, passwordHash: hashSync(env.INIT_PARTNER_PASSWORD, env.SALT_ROUND), roleId: roleAdmin!.id});
-  } else logger.warn(`???????????? "${env.INIT_PARTNER_LOGIN}"(partner) ??? ??????????`);
+  } else logger.warn(`Пользователь "${env.INIT_PARTNER_LOGIN}"(partner) уже существует`);
 
   candidate = await UserModel.findOne({search: [{key: "login", value: env.INIT_OPERATOR_LOGIN}]});
   if (!candidate) {
-    logger.info(`???????? ???????????? "${env.INIT_OPERATOR_LOGIN}"(partner)`);
+    logger.info(`Создание пользователя "${env.INIT_OPERATOR_LOGIN}"(partner)`);
     let roleAdmin = await RoleModel.findOne({search: [{key: "name", value: "partner"}]});
     await UserModel.add({login: env.INIT_OPERATOR_LOGIN, passwordHash: hashSync(env.INIT_OPERATOR_PASSWORD, env.SALT_ROUND), roleId: roleAdmin!.id});
-  } else logger.warn(`???????????? "${env.INIT_OPERATOR_LOGIN}"(partner) ??? ??????????`);
+  } else logger.warn(`Пользователь "${env.INIT_OPERATOR_LOGIN}"(partner) уже существует`);
 };
