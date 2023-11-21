da9aa73817098a14e8f64b0ca7edc766a95753e8
SergeyL-club <damnazzel@gmail.com>
2023-11-21 22:20:46 +0500

    rename get to post

--- src/routers/v1/user.router.ts
+++ src/routers/v1/user.router.ts
@@ -7,8 +7,8 @@ import {authMiddlewareRequest} from "../../middleware/auth.middleware";
 export default async function callback(fastify: FastifyInstance, opts: FastifyPluginOptions, next: (err?: Error | undefined) => void) {
   fastify.withTypeProvider<ZodTypeProvider>().post("/auth", {schema: {body: authorisationBody, response: authorisationResponse}}, userController.authorisation);
   fastify.withTypeProvider<ZodTypeProvider>().get("/testAuth", {preHandler: authMiddlewareRequest.bind(null, [])}, (request, reply) => reply.status(200).send({}));
-  fastify.withTypeProvider<ZodTypeProvider>().get("/refresh", {schema: {response: refreshResponse}}, userController.refresh);
-  fastify.withTypeProvider<ZodTypeProvider>().get("/logout", {schema: {response: logoutResponse}}, userController.logout);
+  fastify.withTypeProvider<ZodTypeProvider>().post("/refresh", {schema: {response: refreshResponse}}, userController.refresh);
+  fastify.withTypeProvider<ZodTypeProvider>().post("/logout", {schema: {response: logoutResponse}}, userController.logout);
   next();
 }
 
