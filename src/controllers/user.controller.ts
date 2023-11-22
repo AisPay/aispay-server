51a6379584881a65732cdf44b108e5af29633f7d
SergeyL-club <damnazzel@gmail.com>
2023-11-22 17:28:14 +0500

    setting cookie

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -11,7 +11,7 @@ class UserController {
 
     const {body} = await userService.authorisation(login, password);
 
-    reply.cookie("refreshToken", body.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000});
+    reply.cookie("refreshToken", body.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, sameSite: "none", secure: "auto"});
 
     return reply.status(200).send(body);
   }
@@ -36,7 +36,7 @@ class UserController {
 
     const {body} = await userService.refresh(accessToken, refreshToken);
 
-    reply.cookie("refreshToken", body.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000});
+    reply.cookie("refreshToken", body.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, sameSite: "none", secure: "auto"});
 
     reply.status(200).send(body);
   }
